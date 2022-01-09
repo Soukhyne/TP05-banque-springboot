35a040838f3a2e1ca3eb6265039eea4091a41dca
madeline <madeline.lafont@gmail.com>
2022-01-09 17:06:07 +0100

    1er commit

---
+++ src/main/java/fr/diginamic/tpjpa05/TpJpa05Application.java
@@ -0,0 +1,13 @@
+package fr.diginamic.tpjpa05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpJpa05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpJpa05Application.class, args);
+	}
+
+}
